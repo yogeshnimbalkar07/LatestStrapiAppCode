7f25647fceb4ea8efd23ba702f62cbadcd47503a
yogeshnimbalkar07 <162571678+yogeshnimbalkar07@users.noreply.github.com>
2024-06-27 15:41:15 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,18 +2,17 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf', // or 'mongoose' for MongoDB
+      connector: 'bookshelf',
       settings: {
-        client: 'postgres',  // Change this to 'mysql', 'sqlite', etc., based on your database
+        client: 'postgres',  // or 'mysql' or 'sqlite' depending on your database
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
         username: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi_password'),
-        ssl: env.bool('DATABASE_SSL', false),
       },
       options: {
-        ssl: env.bool('DATABASE_SSL', false), // You might need to adjust SSL settings based on your database setup
+        ssl: env.bool('DATABASE_SSL', false),
       },
     },
   },
