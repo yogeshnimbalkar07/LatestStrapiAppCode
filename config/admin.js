4cb37690c170f04e477110133ee1413912e2bba6
yogeshnimbalkar07 <162571678+yogeshnimbalkar07@users.noreply.github.com>
2024-06-27 15:56:42 +0530

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('qy9gtpSIOn6l5A2GE0rwRPejzySWXrjGHW1D+t81Iz4='),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
